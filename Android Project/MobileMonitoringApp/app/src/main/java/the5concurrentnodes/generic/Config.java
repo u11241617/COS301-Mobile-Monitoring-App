61808826573502c5b4ae81aca10c58e6bb543b13
u11241617 <u11241617@tuks.co.za>
2015-07-06 04:23:51 +0200

    Initial sms observer commit

--- Android Project/MobileMonitoringApp/app/src/main/java/the5concurrentnodes/generic/Config.java
+++ Android Project/MobileMonitoringApp/app/src/main/java/the5concurrentnodes/generic/Config.java
@@ -4,4 +4,8 @@ public class Config {
 
     public final static String HOST = "10.0.2.2";
     public final static String REST_API = "http://"+ HOST + "/Dashboard/resources";
+
+    public static final String CONTENT_SMS_URI = "content://sms";
+    public static final int SMS_RECEIVED = 1; //Values that represent received sms
+    public static final int SMS_SENT = 2; //Values that represent a sent sms
 }
---
+++ Android Project/MobileMonitoringApp/app/src/main/java/the5concurrentnodes/mmaData/sms/SmsService.java
@@ -0,0 +1,64 @@
+package the5concurrentnodes.mmaData.sms;
+
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.IBinder;
+
+import java.util.Date;
+
+import the5concurrentnodes.generic.Config;
+
+public class SmsService extends Service {
+
+    private  boolean serviceInitialized;
+    private ContentResolver contentResolver;
+    private SmsObserver smsObserver;
+
+    @Override
+    public IBinder onBind(Intent intent) { return  null;}
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        if(!serviceInitialized) {
+
+            smsObserver = new SmsObserver(getApplicationContext());
+            contentResolver = getBaseContext().getContentResolver();
+            contentResolver.registerContentObserver(Uri.parse(Config.CONTENT_SMS_URI), true, smsObserver);
+            serviceInitialized = true;
+        }
+
+        return  START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        contentResolver.unregisterContentObserver(smsObserver);
+        serviceInitialized = false;
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+        restartService();
+    }
+
+    private void restartService() {
+
+        Intent intent = new Intent(this, SmsService.class);
+        PendingIntent pendingIntent = PendingIntent.getService(this, 0, intent, 0);
+        long now = new Date().getTime();
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, now + 1000, pendingIntent);
+    }
+
+}
