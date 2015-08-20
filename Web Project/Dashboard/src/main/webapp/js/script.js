6a2f3f6f106b9694647da844e5dafd36bac361d3
MattNel <matt.nel@hotmail.com>
2015-08-20 23:21:41 +0200

    dashboard login update

--- Web Project/Dashboard/src/main/webapp/js/script.js
+++ Web Project/Dashboard/src/main/webapp/js/script.js
@@ -1,5 +1,14 @@
-
-$(document).ready(function() {
-
-
-});
+function showPassword() 
+{
+    var password = $('#pass').attr('type');
+    
+    if(password != 'text')
+	{
+        $('.checkbox').addClass('show');
+        $('#pass').attr('type', 'text');
+    } else
+	{
+        $('.checkbox').removeClass('show');
+        $('#pass').attr('type', 'password');
+    } 
+}
