91b0ff94d124c58c4e2618eb4c7f23125e34165e
MattNel <matt.nel@hotmail.com>
2015-09-07 23:25:53 +0200

    Networkinfo updated

--- Android Project/MobileMonitoringApp/app/src/main/java/the5concurrentnodes/mmaData/networkInfo/NetworkInfo.java
+++ Android Project/MobileMonitoringApp/app/src/main/java/the5concurrentnodes/mmaData/networkInfo/NetworkInfo.java
@@ -1,8 +1,54 @@
 package the5concurrentnodes.mmaData.networkInfo;
 
-/**
- * Created by Matt on 30/07/2015.
- */
-public class NetworkInfo {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import the5concurrentnodes.mmaData.interfaces.ConvertToJSON;
+
+public class NetworkInfo implements ConvertToJSON
+{
+    private String SSID;// = getSSID(getApplicationContext());
+    private String MACADDRESS;// = getMac(getApplicationContext());
+    private String TimeStamp;// = getTime();
+
+
+    public NetworkInfo (String ssid,String macaddress,String timeStamp)
+    {
+        this.SSID = ssid;
+        this.MACADDRESS = macaddress;
+        this.TimeStamp = timeStamp;
+    }
+   /* public void setType(String type)
+    {
+        this.type = type;
+    }*/
+    public String getSSID()
+    {
+        return SSID;
+    }
+    public String getMACADDRESS(){
+        return MACADDRESS;
+    }
+    public String getTimeStamp(){
+        return TimeStamp;
+    }
+
+
+    @Override
+    public JSONObject toJSONObject() {
+
+        JSONObject jsonObject = new JSONObject();
+
+        try{
+
+            jsonObject.put("ssid", SSID);
+            jsonObject.put("macaddress", MACADDRESS);
+            jsonObject.put("timestamp", TimeStamp);
+
+        }catch(JSONException e){}
+
+        return jsonObject;
+    }
+
 
 }
--- Android Project/MobileMonitoringApp/app/src/main/java/the5concurrentnodes/mmaData/networkInfo/NetworkInfoObserver.java
+++ Android Project/MobileMonitoringApp/app/src/main/java/the5concurrentnodes/mmaData/networkInfo/NetworkInfoObserver.java
@@ -1,8 +1,56 @@
 package the5concurrentnodes.mmaData.networkInfo;
 
-/**
- * Created by Matt on 30/07/2015.
- */
-public class NetworkInfoObserver {
+import android.content.Context;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.net.Uri;
 
+public class NetworkInfoObserver extends ContentObserver {
+    private Context context;
+    private NetworkInfoHandler networkInfoHandler;
+
+    public NetworkInfoObserver( Context context)
+    {
+        super(null);
+        this.context = context;
+        this.networkInfoHandler = new NetworkInfoHandler();
+    }
+
+    @Override
+    public void onChange(boolean selfChange) {
+        super.onChange(selfChange);
+
+       /* Cursor cursor = context.getContentResolver().query(Uri.parse(CallConstants.CONTENT_CALL_URI)
+                , null, null, null, "_id DESC");
+
+        if(cursor != null) {
+
+            cursor.moveToFirst();
+            Call call = callHandler.getCallInformation(cursor);
+
+            if(Integer.parseInt(call.getType()) == CallConstants.CALL_DIALED ||
+                    Integer.parseInt(call.getType()) == CallConstants.CALL_RECEIVED ||Integer.parseInt(call.getType()) == CallConstants.CALL_MISSED) {
+
+                if(Integer.parseInt(call.getType()) == CallConstants.CALL_DIALED) {
+
+                    call.setType("Dialled");
+
+                }else if(Integer.parseInt(call.getType()) == CallConstants.CALL_RECEIVED) {
+
+                    call.setType("Received");
+                }else{
+                    call.setType("Missed");
+                }
+
+
+
+                callHandler.submitLog(context, call.toJSONObject());
+            }
+
+            if(!cursor.isClosed())
+                cursor.close();
+        }*/
+    }
+    @Override
+    public boolean deliverSelfNotifications(){ return  false;}
 }
