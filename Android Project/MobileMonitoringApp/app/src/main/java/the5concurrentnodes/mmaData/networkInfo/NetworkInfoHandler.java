3c52d4e524b95fbc7df76da45608e0b72f7eefdc
MattNel <matt.nel@hotmail.com>
2015-09-08 23:46:39 +0200

    update to networkinfo

--- Android Project/MobileMonitoringApp/app/src/main/java/the5concurrentnodes/mmaData/networkInfo/NetworkInfoHandler.java
+++ Android Project/MobileMonitoringApp/app/src/main/java/the5concurrentnodes/mmaData/networkInfo/NetworkInfoHandler.java
@@ -1,8 +1,26 @@
 package the5concurrentnodes.mmaData.networkInfo;
 
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+
 /**
  * Created by Matt on 30/07/2015.
  */
-public class NetworkInfoHandler {
+public class NetworkInfoHandler extends Activity {
+
+    private BroadcastReceiver myWifiReceiver
+            = new BroadcastReceiver(){
+
+        @Override
+        public void onReceive(Context arg0, Intent arg1) {
+            // TODO Auto-generated method stub
+            NetworkInfo networkInfo = (NetworkInfo) arg1.getParcelableExtra(ConnectivityManager.EXTRA_NETWORK_INFO);
+            if(networkInfo.getType() == ConnectivityManager.TYPE_WIFI){
+                DisplayWifiState();
+            }
+        }};
 
 }
--- Android Project/MobileMonitoringApp/app/src/main/java/the5concurrentnodes/mmaData/networkInfo/NetworkInfoObserver.java
+++ Android Project/MobileMonitoringApp/app/src/main/java/the5concurrentnodes/mmaData/networkInfo/NetworkInfoObserver.java
@@ -20,36 +20,7 @@ public class NetworkInfoObserver extends ContentObserver {
     public void onChange(boolean selfChange) {
         super.onChange(selfChange);
 
-       /* Cursor cursor = context.getContentResolver().query(Uri.parse(CallConstants.CONTENT_CALL_URI)
-                , null, null, null, "_id DESC");
 
-        if(cursor != null) {
-
-            cursor.moveToFirst();
-            Call call = callHandler.getCallInformation(cursor);
-
-            if(Integer.parseInt(call.getType()) == CallConstants.CALL_DIALED ||
-                    Integer.parseInt(call.getType()) == CallConstants.CALL_RECEIVED ||Integer.parseInt(call.getType()) == CallConstants.CALL_MISSED) {
-
-                if(Integer.parseInt(call.getType()) == CallConstants.CALL_DIALED) {
-
-                    call.setType("Dialled");
-
-                }else if(Integer.parseInt(call.getType()) == CallConstants.CALL_RECEIVED) {
-
-                    call.setType("Received");
-                }else{
-                    call.setType("Missed");
-                }
-
-
-
-                callHandler.submitLog(context, call.toJSONObject());
-            }
-
-            if(!cursor.isClosed())
-                cursor.close();
-        }*/
     }
     @Override
     public boolean deliverSelfNotifications(){ return  false;}
