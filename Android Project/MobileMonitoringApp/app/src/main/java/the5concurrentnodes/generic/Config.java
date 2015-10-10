42cd462735e1471b0129c7fada0ed1c3638c94f2
Thabang Michael Letageng <Thabangletageng@gmail.com>
2015-10-10 15:16:49 +0200

    Privacy, terms of use and about us page update

--- Android Project/MobileMonitoringApp/app/src/main/java/the5concurrentnodes/generic/Config.java
+++ Android Project/MobileMonitoringApp/app/src/main/java/the5concurrentnodes/generic/Config.java
@@ -2,7 +2,7 @@ package the5concurrentnodes.generic;
 
 public class Config {
 
-    public final static String HOST = "d761e4eb.ngrok.io";
+    public final static String HOST = "b19842bb.ngrok.io";
     public final static String REST_API = "http://"+ HOST + "/Dashboard/resources";
 
 }
